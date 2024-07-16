8ad880c1a519b26c133bbb54f5d37d43419db42a
Yevhen Sydorov <105035206+Sokiryanin@users.noreply.github.com>
2024-07-16 16:11:45 +0200

    add header, main, how_page

---
+++ src/js/slider.js
@@ -0,0 +1,17 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.slider-main-block', {
+  // Optional parameters
+
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.body-main-block__arrow.swiper-button-prev',
+    prevEl: '.body-main-block__arrow.swiper-button-next',
+  },
+});
